0828b2aad0fcf3c143d6202e236847fe9c5930fd
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2020-10-28 19:33:19 +0100

    included servletExample

---
+++ servletExample/src/main/java/com/magnotta/TestServlet.java
@@ -0,0 +1,26 @@
+package com.magnotta;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("text/plain;charset=UTF-8");
+        
+        ServletOutputStream out = response.getOutputStream();
+        
+        out.print("Hello from TestServlet");
+    }
+
+}
---
+++ servletExample/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+
+<body>
+    <h2>Hello World from index.jsp!</h2>
+    <a href="test">Call TestServlet</a>
+</body>
+
+</html>
