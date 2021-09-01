bf32e8f6c483bf9f7b252b628c10755782dd6e91
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2021-09-01 16:35:32 +0200

    code indentation fix

--- quarkus-order-processor/src/main/java/org/gmagnotta/observer/CommandObserver.java
+++ quarkus-order-processor/src/main/java/org/gmagnotta/observer/CommandObserver.java
@@ -41,259 +41,260 @@ import io.quarkus.runtime.StartupEvent;
 
 @ApplicationScoped
 public class CommandObserver implements MessageListener {
-	
+
 	private static final Logger LOGGER = Logger.getLogger(CommandObserver.class);
-	
+
 	private static final String COMMAND_QUEUE = "orderCommand";
-	
+
 	private static final String ORDER_CHANGED_QUEUE = "orderChanged";
-	
+
 	private static final String INVALID_MESSAGE_QUEUE = "invalidMessage";
 
-    @Inject
-    ConnectionFactory connectionFactory;
-    
-    @Inject
-    EntityManager entityManager;
-    
-    JMSContext context;
-    
-    JMSConsumer consumer;
-    
-    Queue invalidMessageQueue;
-    
+	@Inject
+	ConnectionFactory connectionFactory;
+
+	@Inject
+	EntityManager entityManager;
+
+	JMSContext context;
+
+	JMSConsumer consumer;
+
+	Queue invalidMessageQueue;
+
 	@PostConstruct
 	private void init() {
-		
+
 		context = connectionFactory.createContext();
-    	
-     	Queue commandQueue = context.createQueue(COMMAND_QUEUE);
-     	
-     	consumer = context.createConsumer(commandQueue);
-     	
-     	invalidMessageQueue = context.createQueue(INVALID_MESSAGE_QUEUE);
+
+		Queue commandQueue = context.createQueue(COMMAND_QUEUE);
+
+		consumer = context.createConsumer(commandQueue);
+
+		invalidMessageQueue = context.createQueue(INVALID_MESSAGE_QUEUE);
 
 	}
-    
-     void onStart(@Observes StartupEvent ev) throws JMSException {
 
-     	consumer.setMessageListener(this);
-     	
-     	LOGGER.info("Consumer started");
+	void onStart(@Observes StartupEvent ev) throws JMSException {
 
-    }
+		consumer.setMessageListener(this);
 
-    void onStop(@Observes ShutdownEvent ev) throws JMSException {
-    	
-    	consumer.close();
-    	
-    	context.close();
-    	
-    	LOGGER.info("Consumer stopped");
-    	
-    }
+		LOGGER.info("Consumer started");
+
+	}
+
+	void onStop(@Observes ShutdownEvent ev) throws JMSException {
+
+		consumer.close();
+
+		context.close();
+
+		LOGGER.info("Consumer stopped");
+
+	}
 
 	@Override
 	@Transactional
 	public void onMessage(Message message) {
-		
+
 		try {
-			
-            if (message == null || !(message instanceof TextMessage)) {
-            	
-            	LOGGER.warn("Received unexpected message");
-            	
-            	context.createProducer().send(invalidMessageQueue, message);
-            	
-            } else {
-            	
-            	TextMessage requestMessage = (TextMessage) message;
-            	
+
+			if (message == null || !(message instanceof TextMessage)) {
+
+				LOGGER.warn("Received unexpected message");
+
+				context.createProducer().send(invalidMessageQueue, message);
+
+			} else {
+
+				TextMessage requestMessage = (TextMessage) message;
+
 //            	LOGGER.info("Received content " + requestMessage.getText());
-            	
-            	OrderCommandRequest orderCommandRequest = Utils.unmarshall(OrderCommandRequest.class, requestMessage.getText());
-            	
-	            LOGGER.info("Received message id " + message.getJMSMessageID());
-	            
-	            if (orderCommandRequest.getOrderCommandEnum().equals(OrderCommandRequestEnum.ORDER_RECEIVED)) {
-	            	
-	            	org.gmagnotta.jaxb.Order jaxbOrder = orderCommandRequest.getOrder();
-	            	
-	            	org.gmagnotta.model.Order jpaOrder = convertoToJpaOrder(jaxbOrder);
-	            	
-	            	entityManager.persist(jpaOrder);
-	                for (org.gmagnotta.model.LineItem l : jpaOrder.getLineItems()) {
-	                    entityManager.persist(l);
-	                }
-	                
-	                LOGGER.info("Persisted order id " + jpaOrder.getId());
-	                
-	                OrderChangeEvent event = OrderChangeEvent.newBuilder()
-	                		.setType(OrderChangeEvent.EventType.ORDER_CREATED)
-	                		.setOrder(Utils.convertToProtobuf(jpaOrder))
-	                        .build();
-	                
-	                BytesMessage bmessage = context.createBytesMessage();
-	                bmessage.writeBytes(event.toByteArray());
-	                
-	            	Queue orderChangedQueue = context.createQueue(ORDER_CHANGED_QUEUE);
-	            	context.createProducer().send(orderChangedQueue, bmessage);
-	            	
-	            	LOGGER.info("Sent event message");
-		    	
-	            } else if (orderCommandRequest.getOrderCommandEnum().equals(OrderCommandRequestEnum.GET_TOP_ORDERS)) {
-	            	
-	            	Query query = entityManager.createNativeQuery("select sum(quantity) as items, orders from line_item group by orders order by items desc, orders asc");
-	            	
-	            	List<Object> queryResult = query.setMaxResults(10).getResultList();
-	            	
-	            	Iterator<Object> iterator = queryResult.iterator();
-	            	
-	            	List<TopValue> result = new ArrayList<TopValue>();
-	            	
-	            	while (iterator.hasNext()) {
-	            		
-	            		Object obj = iterator.next();
-	            		
-	            		Object[] cast = (Object[]) obj;
-	            		
-	            		TopValue top = new TopValue();
-	            		BigInteger b = (BigInteger) cast[0];
-	            		Integer i = (Integer) cast[1];
-	            		
-	            		top.setId(b.intValue());
-	            		top.setValue(i);
-	            		result.add(top);
-	            	}
-	            	
-	            	OrderCommandResponse responseMessage = new OrderCommandResponse();
-	            	responseMessage.setStatus("200");
-	            	responseMessage.getTopvalue().addAll(result);
-	            	
-	            	StringWriter s = Utils.marshall(new ObjectFactory().createOrderCommandResponse(responseMessage));
-	            	
-	            	TextMessage response = context.createTextMessage(s.toString());
-	            	response.setJMSCorrelationID(requestMessage.getJMSMessageID());
-	            	
-	            	Destination responseChannel = requestMessage.getJMSReplyTo();
-	            	
-	            	context.createProducer().send(responseChannel, response);
-	            	
-	            	LOGGER.info("Sent response");
-	            	
-	            } else if (orderCommandRequest.getOrderCommandEnum().equals(OrderCommandRequestEnum.GET_TOP_ITEMS)) {
-	            	
-	            	Query query = entityManager.createNativeQuery("select item, sum(quantity) from line_item group by item order by sum(quantity) desc");
-	            	
-	            	List<Object> queryResult = query.setMaxResults(10).getResultList();
-	            	
-	            	Iterator<Object> iterator = queryResult.iterator();
-	            	
-	            	List<TopValue> result = new ArrayList<TopValue>();
-	            	
-	            	while (iterator.hasNext()) {
-	            		
-	            		Object obj = iterator.next();
-	            		
-	            		Object[] cast = (Object[]) obj;
-	            		
-	            		TopValue top = new TopValue();
-	            		Integer b = (Integer) cast[0];
-	            		BigInteger i = (BigInteger) cast[1];
-	            		
-	            		top.setId(b.intValue());
-	            		top.setValue(i.intValue());
-	            		result.add(top);
-	            	}
-	            	
-	            	OrderCommandResponse responseMessage = new OrderCommandResponse();
-	            	responseMessage.setStatus("200");
-	            	responseMessage.getTopvalue().addAll(result);
-	            	
-	            	StringWriter s = Utils.marshall(new ObjectFactory().createOrderCommandResponse(responseMessage));
-	            	
-	            	TextMessage response = context.createTextMessage(s.toString());
-	            	response.setJMSCorrelationID(requestMessage.getJMSMessageID());
-	            	
-	            	Destination responseChannel = requestMessage.getJMSReplyTo();
-	            	
-	            	context.createProducer().send(responseChannel, response);
-	            	
-	            	LOGGER.info("Sent response");
-	            	
-	            } else if (orderCommandRequest.getOrderCommandEnum().equals(OrderCommandRequestEnum.RESET)) {
-	            	
-	            	Query query = entityManager.createNativeQuery("delete from line_item; delete from orders;");
-	            	
-	            	query.executeUpdate();
-	            	
-	            	OrderCommandResponse responseMessage = new OrderCommandResponse();
-	            	responseMessage.setStatus("200");
-	            	
-	            	StringWriter s = Utils.marshall(new ObjectFactory().createOrderCommandResponse(responseMessage));
-	            	
-	            	TextMessage response = context.createTextMessage(s.toString());
-	            	response.setJMSCorrelationID(requestMessage.getJMSMessageID());
-	            	
-	            	Destination responseChannel = requestMessage.getJMSReplyTo();
-	            	
-	            	context.createProducer().send(responseChannel, response);
-	            	
-	            	LOGGER.info("Sent response");
-	            	
-	            } else {
-	            	
-	            	LOGGER.warn("Unknown operation");
-	            	
-	            	context.createProducer().send(invalidMessageQueue, message);
-	            	
-	            }
-            
-            }
-            
-        } catch (Exception e) {
-        	
-        	LOGGER.error("An exception occurred during message processing", e);
-        	
-        }
-		
+
+				OrderCommandRequest orderCommandRequest = Utils.unmarshall(OrderCommandRequest.class,
+						requestMessage.getText());
+
+				LOGGER.info("Received message id " + message.getJMSMessageID());
+
+				if (orderCommandRequest.getOrderCommandEnum().equals(OrderCommandRequestEnum.ORDER_RECEIVED)) {
+
+					org.gmagnotta.jaxb.Order jaxbOrder = orderCommandRequest.getOrder();
+
+					org.gmagnotta.model.Order jpaOrder = convertoToJpaOrder(jaxbOrder);
+
+					entityManager.persist(jpaOrder);
+					for (org.gmagnotta.model.LineItem l : jpaOrder.getLineItems()) {
+						entityManager.persist(l);
+					}
+
+					LOGGER.info("Persisted order id " + jpaOrder.getId());
+
+					OrderChangeEvent event = OrderChangeEvent.newBuilder()
+							.setType(OrderChangeEvent.EventType.ORDER_CREATED)
+							.setOrder(Utils.convertToProtobuf(jpaOrder)).build();
+
+					BytesMessage bmessage = context.createBytesMessage();
+					bmessage.writeBytes(event.toByteArray());
+
+					Queue orderChangedQueue = context.createQueue(ORDER_CHANGED_QUEUE);
+					context.createProducer().send(orderChangedQueue, bmessage);
+
+					LOGGER.info("Sent event message");
+
+				} else if (orderCommandRequest.getOrderCommandEnum().equals(OrderCommandRequestEnum.GET_TOP_ORDERS)) {
+
+					Query query = entityManager.createNativeQuery(
+							"select sum(quantity) as items, orders from line_item group by orders order by items desc, orders asc");
+
+					List<Object> queryResult = query.setMaxResults(10).getResultList();
+
+					Iterator<Object> iterator = queryResult.iterator();
+
+					List<TopValue> result = new ArrayList<TopValue>();
+
+					while (iterator.hasNext()) {
+
+						Object obj = iterator.next();
+
+						Object[] cast = (Object[]) obj;
+
+						TopValue top = new TopValue();
+						BigInteger b = (BigInteger) cast[0];
+						Integer i = (Integer) cast[1];
+
+						top.setId(b.intValue());
+						top.setValue(i);
+						result.add(top);
+					}
+
+					OrderCommandResponse responseMessage = new OrderCommandResponse();
+					responseMessage.setStatus("200");
+					responseMessage.getTopvalue().addAll(result);
+
+					StringWriter s = Utils.marshall(new ObjectFactory().createOrderCommandResponse(responseMessage));
+
+					TextMessage response = context.createTextMessage(s.toString());
+					response.setJMSCorrelationID(requestMessage.getJMSMessageID());
+
+					Destination responseChannel = requestMessage.getJMSReplyTo();
+
+					context.createProducer().send(responseChannel, response);
+
+					LOGGER.info("Sent response");
+
+				} else if (orderCommandRequest.getOrderCommandEnum().equals(OrderCommandRequestEnum.GET_TOP_ITEMS)) {
+
+					Query query = entityManager.createNativeQuery(
+							"select item, sum(quantity) from line_item group by item order by sum(quantity) desc");
+
+					List<Object> queryResult = query.setMaxResults(10).getResultList();
+
+					Iterator<Object> iterator = queryResult.iterator();
+
+					List<TopValue> result = new ArrayList<TopValue>();
+
+					while (iterator.hasNext()) {
+
+						Object obj = iterator.next();
+
+						Object[] cast = (Object[]) obj;
+
+						TopValue top = new TopValue();
+						Integer b = (Integer) cast[0];
+						BigInteger i = (BigInteger) cast[1];
+
+						top.setId(b.intValue());
+						top.setValue(i.intValue());
+						result.add(top);
+					}
+
+					OrderCommandResponse responseMessage = new OrderCommandResponse();
+					responseMessage.setStatus("200");
+					responseMessage.getTopvalue().addAll(result);
+
+					StringWriter s = Utils.marshall(new ObjectFactory().createOrderCommandResponse(responseMessage));
+
+					TextMessage response = context.createTextMessage(s.toString());
+					response.setJMSCorrelationID(requestMessage.getJMSMessageID());
+
+					Destination responseChannel = requestMessage.getJMSReplyTo();
+
+					context.createProducer().send(responseChannel, response);
+
+					LOGGER.info("Sent response");
+
+				} else if (orderCommandRequest.getOrderCommandEnum().equals(OrderCommandRequestEnum.RESET)) {
+
+					Query query = entityManager.createNativeQuery("delete from line_item; delete from orders;");
+
+					query.executeUpdate();
+
+					OrderCommandResponse responseMessage = new OrderCommandResponse();
+					responseMessage.setStatus("200");
+
+					StringWriter s = Utils.marshall(new ObjectFactory().createOrderCommandResponse(responseMessage));
+
+					TextMessage response = context.createTextMessage(s.toString());
+					response.setJMSCorrelationID(requestMessage.getJMSMessageID());
+
+					Destination responseChannel = requestMessage.getJMSReplyTo();
+
+					context.createProducer().send(responseChannel, response);
+
+					LOGGER.info("Sent response");
+
+				} else {
+
+					LOGGER.warn("Unknown operation");
+
+					context.createProducer().send(invalidMessageQueue, message);
+
+				}
+
+			}
+
+		} catch (Exception e) {
+
+			LOGGER.error("An exception occurred during message processing", e);
+
+		}
+
 	}
-	
+
 	private org.gmagnotta.model.Order convertoToJpaOrder(org.gmagnotta.jaxb.Order order) {
-		
+
 		long sum = 0;
-		
+
 		org.gmagnotta.model.Order jpaOrder = new org.gmagnotta.model.Order();
-		
+
 		jpaOrder.setCreationDate(order.getCreationDate().toGregorianCalendar().getTime());
 		jpaOrder.setExternalOrderId(order.getExternalOrderId());
-		
-		List<LineItem> lineItems = order.getLineItem();
-		
-		for (LineItem l : lineItems) {
-            
-    		Item i = l.getItem();
-            
-        	Query q = entityManager.createNamedQuery("getItemById", org.gmagnotta.model.Item.class);
-        	q.setParameter("id", i.getId());
-        	
-        	org.gmagnotta.model.Item jpaItem = (org.gmagnotta.model.Item) q.getSingleResult();
 
-        	org.gmagnotta.model.LineItem jpaLineItem = new org.gmagnotta.model.LineItem();
-        	jpaLineItem.setItem(jpaItem);
-        	jpaLineItem.setOrder(jpaOrder);
-        	jpaLineItem.setQuantity(l.getQuantity());
-        	jpaLineItem.setPrice(jpaItem.getPrice());
-        	
-        	jpaOrder.addLineItem(jpaLineItem);
-            
-        	sum += jpaItem.getPrice().multiply(BigDecimal.valueOf(jpaLineItem.getQuantity())).longValue();
-        }
-    	
+		List<LineItem> lineItems = order.getLineItem();
+
+		for (LineItem l : lineItems) {
+
+			Item i = l.getItem();
+
+			Query q = entityManager.createNamedQuery("getItemById", org.gmagnotta.model.Item.class);
+			q.setParameter("id", i.getId());
+
+			org.gmagnotta.model.Item jpaItem = (org.gmagnotta.model.Item) q.getSingleResult();
+
+			org.gmagnotta.model.LineItem jpaLineItem = new org.gmagnotta.model.LineItem();
+			jpaLineItem.setItem(jpaItem);
+			jpaLineItem.setOrder(jpaOrder);
+			jpaLineItem.setQuantity(l.getQuantity());
+			jpaLineItem.setPrice(jpaItem.getPrice());
+
+			jpaOrder.addLineItem(jpaLineItem);
+
+			sum += jpaItem.getPrice().multiply(BigDecimal.valueOf(jpaLineItem.getQuantity())).longValue();
+		}
+
 		jpaOrder.setAmount(BigDecimal.valueOf(sum));
-    	return jpaOrder;
-		
+		return jpaOrder;
+
 	}
-	
-	
+
 }
