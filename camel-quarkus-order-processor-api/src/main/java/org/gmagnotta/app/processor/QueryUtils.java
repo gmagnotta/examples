e81ef3b32b4ff01e8faaf72f8843289c87f44d1b
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2021-10-12 14:10:00 +0200

    code refactory to remove JMS stuff from code and let use camel component

--- camel-quarkus-order-processor-api/src/main/java/org/gmagnotta/app/processor/QueryUtils.java
+++ camel-quarkus-order-processor-api/src/main/java/org/gmagnotta/app/processor/QueryUtils.java
@@ -1,28 +1,17 @@
 package org.gmagnotta.app.processor;
 
-import java.io.StringWriter;
 import java.math.BigInteger;
 import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.TimeoutException;
 
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSConsumer;
-import javax.jms.JMSContext;
-import javax.jms.Message;
-import javax.jms.Queue;
-import javax.jms.TextMessage;
 
 import org.apache.camel.Exchange;
 import org.eclipse.microprofile.opentracing.Traced;
 import org.gmagnotta.jaxb.Aggregateditemtype;
 import org.gmagnotta.jaxb.Aggregatedordertype;
 import org.gmagnotta.jaxb.Aggregationtype;
-import org.gmagnotta.jaxb.ObjectFactory;
 import org.gmagnotta.jaxb.OrderCommandRequest;
 import org.gmagnotta.jaxb.OrderCommandRequestEnum;
 import org.gmagnotta.jaxb.OrderCommandResponse;
@@ -40,249 +29,132 @@ public class QueryUtils {
 	
 	private static final Logger LOGGER = Logger.getLogger(QueryUtils.class);
 	
-	private static final String COMMAND_QUEUE = "orderCommand";
+	public void prepareGetTopOrders(Exchange exchange) throws Exception {
 
-	@Inject
-    ConnectionFactory connectionFactory;
-	
-	JMSContext context;
-    
-    JMSConsumer consumer;
-    
-    @PostConstruct
-	private void init() {
-    	
-    	context = connectionFactory.createContext();
-    
-    }
-	
-	/**
-	 * Returns top 10 Orders with maximum items
-	 */
-    public void getTopOrders(Exchange exchange) throws Exception {
-    	
-    	Queue responseQueue = context.createTemporaryQueue();
-    	Queue requestQueue = context.createQueue(COMMAND_QUEUE);
-    	
-    	OrderCommandRequest requestMessage = new OrderCommandRequest();
-    	requestMessage.setOrderCommandEnum(OrderCommandRequestEnum.GET_TOP_ORDERS);
-    	
-    	StringWriter s = Utils.marshall(new ObjectFactory().createOrderCommandRequest(requestMessage));
-    	
-    	TextMessage textMessage = context.createTextMessage(s.toString());
-    	textMessage.setJMSReplyTo(responseQueue);
-    	textMessage.setJMSExpiration(10000L);
-    	
-    	LOGGER.info("Sending message " + textMessage);
-    	
-    	context.createProducer().send(requestQueue, textMessage);
-    	
-    	JMSConsumer consumer = context.createConsumer(responseQueue);
-    	Message message = consumer.receive(10000L);
-        if (message == null) {
-        	LOGGER.warn("Received null reply");
-        	throw new TimeoutException();
-        }
+        OrderCommandRequest request = new OrderCommandRequest();
+        request.setOrderCommandEnum(OrderCommandRequestEnum.GET_TOP_ORDERS);
         
-        if (message.getJMSCorrelationID().equals(textMessage.getJMSMessageID())) {
-        	
-        	String responseContent = message.getBody(String.class);
-        	
-        	OrderCommandResponse responseMessage = Utils.unmarshall(OrderCommandResponse.class, responseContent);
-        	List<TopValue> result = responseMessage.getTopvalue();
-        	
-        	Aggregationtype aggregation = new Aggregationtype();
-        	
-    		Iterator<TopValue> iterator = result.iterator();
-        	while (iterator.hasNext()) {
-        		
-        		TopValue temp = iterator.next();
-        		
-        		Aggregatedordertype order = new Aggregatedordertype();
-        		
-        		int items = temp.getId();
-        		
-        		int orderId = temp.getValue();
-        		
-        		order.setItems(BigInteger.valueOf(items));
-        		order.setOrderid(String.valueOf(orderId));
-        		
-        		aggregation.getAggregatedorder().add(order);
-        		
-        	}
-        	
-        	exchange.getOut().setBody(aggregation);
-        	
-        	LOGGER.info("Received response message ");
-        	
-        } else {
-        	
-        	LOGGER.error("Unknown response message " + message);
-        	
-        	throw new Exception("Unknown response message");
-        	
-        }
-    	
-    }
-	
-	/**
-	 * Return most requested Items
-	 */
-    public void getTopItems(Exchange exchange) throws Exception {
-    	
-    	Queue responseQueue = context.createTemporaryQueue();
-    	Queue requestQueue = context.createQueue(COMMAND_QUEUE);
-    	
-    	OrderCommandRequest requestMessage = new OrderCommandRequest();
-    	requestMessage.setOrderCommandEnum(OrderCommandRequestEnum.GET_TOP_ITEMS);
-    	
-    	StringWriter s = Utils.marshall(new ObjectFactory().createOrderCommandRequest(requestMessage));
-    	
-    	TextMessage textMessage = context.createTextMessage(s.toString());
-    	textMessage.setJMSReplyTo(responseQueue);
-    	textMessage.setJMSExpiration(10000L);
-    	
-    	LOGGER.info("Sending message " + textMessage);
-    	
-    	context.createProducer().send(requestQueue, textMessage);
-    	
-    	JMSConsumer consumer = context.createConsumer(responseQueue);
-    	Message message = consumer.receive(10000L);
-        if (message == null) {
-        	LOGGER.warn("Received null reply");
-        	throw new TimeoutException();
-        }
-        
-        if (message.getJMSCorrelationID().equals(textMessage.getJMSMessageID())) {
-        	
-        	String responseContent = message.getBody(String.class);
-        	
-        	OrderCommandResponse responseMessage = Utils.unmarshall(OrderCommandResponse.class, responseContent);
-        	
-        	List<TopValue> result = responseMessage.getTopvalue();
-        	
-        	Aggregationtype aggregation = new Aggregationtype();
-        	
-        	Iterator<TopValue> iterator = result.iterator();
-        	while (iterator.hasNext()) {
-        		
-        		TopValue temp = iterator.next();
-        		
-        		Aggregateditemtype order = new Aggregateditemtype();
-        		
-        		int items = temp.getId();
-        		
-        		int qty = temp.getValue();
-        		
-        		order.setItem(String.valueOf(items));
-        		order.setQuantity(BigInteger.valueOf(qty));
-        		
-        		aggregation.getAggregateditem().add(order);
-        		
-        	}
-        	
-        	exchange.getOut().setBody(aggregation);
-        	
-        	LOGGER.info("Received response message ");
-        	
-        } else {
-        	
-        	LOGGER.error("Unknown response message " + message);
-        	
-        	throw new Exception("Unknown response message");
-        	
-        }
-    	
-    }
-	
-	public void reset(Exchange exchange) throws Exception {
+        // replace the body of the exchange
+        exchange.getIn().setBody(request);
 
-    	Queue responseQueue = context.createTemporaryQueue();
-    	Queue requestQueue = context.createQueue(COMMAND_QUEUE);
-    	
-    	OrderCommandRequest requestMessage = new OrderCommandRequest();
-    	requestMessage.setOrderCommandEnum(OrderCommandRequestEnum.RESET);
-    	
-    	StringWriter s = Utils.marshall(new ObjectFactory().createOrderCommandRequest(requestMessage));
-    	
-    	TextMessage textMessage = context.createTextMessage(s.toString());
-    	textMessage.setJMSReplyTo(responseQueue);
-    	textMessage.setJMSExpiration(10000L);
-    	
-    	LOGGER.info("Sending message " + textMessage);
-    	
-    	context.createProducer().send(requestQueue, textMessage);
-    	
-    	JMSConsumer consumer = context.createConsumer(responseQueue);
-    	Message message = consumer.receive(10000L);
-        if (message == null) {
-        	LOGGER.warn("Received null reply");
-        	throw new TimeoutException();
-        }
-        
-        if (message.getJMSCorrelationID().equals(textMessage.getJMSMessageID())) {
-        	
-        	String responseContent = message.getBody(String.class);
-        	
-        	OrderCommandResponse responseMessage = Utils.unmarshall(OrderCommandResponse.class, responseContent);
-        	
-        	if (!"200".equalsIgnoreCase(responseMessage.getStatus())) {
-        		throw new Exception("Error");
-        	}
-        		
-        	
-        } else {
-        	
-        	LOGGER.error("Unknown response message " + message);
-        	
-        	throw new Exception("Unknown response message");
-        	
-        }
-	}
-	
-	public void rebuild(Exchange exchange) throws Exception {
-
-    	Queue responseQueue = context.createTemporaryQueue();
-    	Queue requestQueue = context.createQueue(COMMAND_QUEUE);
-    	
-    	OrderCommandRequest requestMessage = new OrderCommandRequest();
-    	requestMessage.setOrderCommandEnum(OrderCommandRequestEnum.REBUILD);
-    	
-    	StringWriter s = Utils.marshall(new ObjectFactory().createOrderCommandRequest(requestMessage));
-    	
-    	TextMessage textMessage = context.createTextMessage(s.toString());
-    	textMessage.setJMSReplyTo(responseQueue);
-    	textMessage.setJMSExpiration(10000L);
-    	
-    	LOGGER.info("Sending message " + textMessage);
-    	
-    	context.createProducer().send(requestQueue, textMessage);
-    	
-    	JMSConsumer consumer = context.createConsumer(responseQueue);
-    	Message message = consumer.receive(10000L);
-        if (message == null) {
-        	LOGGER.warn("Received null reply");
-        	throw new TimeoutException();
-        }
-        
-        if (message.getJMSCorrelationID().equals(textMessage.getJMSMessageID())) {
-        	
-        	String responseContent = message.getBody(String.class);
-        	
-        	OrderCommandResponse responseMessage = Utils.unmarshall(OrderCommandResponse.class, responseContent);
-        	
-        	if (!"200".equalsIgnoreCase(responseMessage.getStatus())) {
-        		throw new Exception("Error");
-        	}
-        		
-        	
-        } else {
-        	
-        	LOGGER.error("Unknown response message " + message);
-        	
-        	throw new Exception("Unknown response message");
-        	
-        }
 	}
 
+	public void decodeGetTopOrders(Exchange exchange) throws Exception {
+
+		String responseContent = exchange.getIn().getBody(String.class);
+        	
+		OrderCommandResponse responseMessage = Utils.unmarshall(OrderCommandResponse.class, responseContent);
+		List<TopValue> result = responseMessage.getTopvalue();
+		
+		Aggregationtype aggregation = new Aggregationtype();
+		
+		Iterator<TopValue> iterator = result.iterator();
+		while (iterator.hasNext()) {
+			
+			TopValue temp = iterator.next();
+			
+			Aggregatedordertype order = new Aggregatedordertype();
+			
+			int items = temp.getId();
+			
+			int orderId = temp.getValue();
+			
+			order.setItems(BigInteger.valueOf(items));
+			order.setOrderid(String.valueOf(orderId));
+			
+			aggregation.getAggregatedorder().add(order);
+			
+		}
+		
+		exchange.getOut().setBody(aggregation);
+	}
+
+	
+	public void prepareGetTopItems(Exchange exchange) throws Exception {
+
+        OrderCommandRequest request = new OrderCommandRequest();
+        request.setOrderCommandEnum(OrderCommandRequestEnum.GET_TOP_ITEMS);
+        
+        // replace the body of the exchange
+        exchange.getIn().setBody(request);
+
+	}
+
+	public void decodeGetTopItems(Exchange exchange) throws Exception {
+    	
+    	
+        String responseContent = exchange.getIn().getBody(String.class);
+        	
+		OrderCommandResponse responseMessage = Utils.unmarshall(OrderCommandResponse.class, responseContent);
+		
+		List<TopValue> result = responseMessage.getTopvalue();
+		
+		Aggregationtype aggregation = new Aggregationtype();
+		
+		Iterator<TopValue> iterator = result.iterator();
+		while (iterator.hasNext()) {
+			
+			TopValue temp = iterator.next();
+			
+			Aggregateditemtype order = new Aggregateditemtype();
+			
+			int items = temp.getId();
+			
+			int qty = temp.getValue();
+			
+			order.setItem(String.valueOf(items));
+			order.setQuantity(BigInteger.valueOf(qty));
+			
+			aggregation.getAggregateditem().add(order);
+			
+		}
+		
+		exchange.getOut().setBody(aggregation);
+		
+    }
+	
+	public void prepareReset(Exchange exchange) throws Exception {
+
+        OrderCommandRequest request = new OrderCommandRequest();
+        request.setOrderCommandEnum(OrderCommandRequestEnum.RESET);
+        
+        // replace the body of the exchange
+        exchange.getIn().setBody(request);
+
+	}
+
+	public void decodeReset(Exchange exchange) throws Exception {
+    	
+        String responseContent = exchange.getIn().getBody(String.class);
+
+		OrderCommandResponse responseMessage = Utils.unmarshall(OrderCommandResponse.class, responseContent);
+        	
+		if (!"200".equalsIgnoreCase(responseMessage.getStatus())) {
+			throw new Exception("Error");
+		}
+
+	}
+	
+	public void prepareRebuild(Exchange exchange) throws Exception {
+
+        OrderCommandRequest request = new OrderCommandRequest();
+        request.setOrderCommandEnum(OrderCommandRequestEnum.REBUILD);
+        
+        // replace the body of the exchange
+        exchange.getIn().setBody(request);
+
+	}
+
+	public void decodeRebuild(Exchange exchange) throws Exception {
+    	
+        String responseContent = exchange.getIn().getBody(String.class);
+
+		OrderCommandResponse responseMessage = Utils.unmarshall(OrderCommandResponse.class, responseContent);
+        	
+		if (!"200".equalsIgnoreCase(responseMessage.getStatus())) {
+			throw new Exception("Error");
+		}
+
+	}
+	
 }
--- camel-quarkus-order-processor-api/src/main/java/org/gmagnotta/app/route/RestRoute.java
+++ camel-quarkus-order-processor-api/src/main/java/org/gmagnotta/app/route/RestRoute.java
@@ -9,6 +9,10 @@ import org.apache.camel.model.rest.RestParamType;
 import org.gmagnotta.jaxb.Aggregationtype;
 import org.jboss.logging.Logger;
 
+import org.apache.camel.converter.jaxb.JaxbDataFormat;
+
+import org.apache.camel.component.activemq.ActiveMQComponent;
+
 @ApplicationScoped
 /**
  *  Camel Route to expose Rest services
@@ -20,16 +24,24 @@ public class RestRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+      JaxbDataFormat jaxbDataFormat = new JaxbDataFormat("org.gmagnotta.jaxb");
+
+      ActiveMQComponent activeMq = new ActiveMQComponent();
+      getContext().addComponent("activemq2", activeMq);
+
     	restConfiguration()
         .component("platform-http")
         .bindingMode(RestBindingMode.auto)
         .contextPath("/api");
    	
-       rest("/topOrders")
+        rest("/topOrders")
         .get().outType(Aggregationtype.class)
         .route()
          .circuitBreaker()
-          .to("bean://queryutils?method=getTopOrders")
+          .to("bean://queryutils?method=prepareGetTopOrders")
+          .marshal(jaxbDataFormat)
+          .to("activemq2:queue:orderCommand?jmsMessageType=Text")
+          .to("bean://queryutils?method=decodeGetTopOrders")
          .onFallback()
            .setBody(constant("Dependant service not available"))
            .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
@@ -38,49 +50,62 @@ public class RestRoute extends RouteBuilder {
         .endRest();
        
        rest("/topItems")
-       .get().outType(Aggregationtype.class)
-       .route()
-       .circuitBreaker()
-        .to("bean://queryutils?method=getTopItems")
-        .onFallback()
-        .setBody(constant("Dependant service not available"))
-        .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
-        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
-        .end()
-       .endRest();
+        .get().outType(Aggregationtype.class)
+        .route()
+         .circuitBreaker()
+          .to("bean://queryutils?method=prepareGetTopItems")
+          .marshal(jaxbDataFormat)
+          .to("activemq2:queue:orderCommand?jmsMessageType=Text")
+          .to("bean://queryutils?method=decodeGetTopItems")
+         .onFallback()
+           .setBody(constant("Dependant service not available"))
+           .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
+           .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
+         .end()
+        .endRest();
 
        rest("/order")
         .post("/generate")
          .param().name("amount").type(RestParamType.query).defaultValue("1").endParam()
-         .route().setHeader(Exchange.HTTP_RESPONSE_CODE, simple("201"))
+         .route()
+          .setHeader(Exchange.HTTP_RESPONSE_CODE, simple("201"))
+          .setBody(constant("Done"))
          .to("bean://generateprocessor")
         .endRest();
        
-       rest("/reset")
-       .post()
+      rest("/reset")
+        .post()
         .route()
-        .circuitBreaker()
-         .to("bean://queryutils?method=reset")
-         .setHeader(Exchange.HTTP_RESPONSE_CODE, simple("201"))
-        .onFallback()
-        .setBody(constant("Dependant service not available"))
-        .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
-        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
-        .end()
-      .endRest();
-       
-       rest("/rebuild")
-       .post()
-       .route()
-       .circuitBreaker()
-        .to("bean://queryutils?method=rebuild")
-        .setHeader(Exchange.HTTP_RESPONSE_CODE, simple("200"))
-       .onFallback()
-        .setBody(constant("Dependant service not available"))
-        .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
-        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
-      .end()
-      .endRest();
+         .circuitBreaker()
+          .to("bean://queryutils?method=prepareReset")
+          .marshal(jaxbDataFormat)
+          .to("activemq2:queue:orderCommand?jmsMessageType=Text")
+          .to("bean://queryutils?method=decodeReset")
+          .setHeader(Exchange.HTTP_RESPONSE_CODE, simple("200"))
+          .setBody(constant("Done"))
+         .onFallback()
+           .setBody(constant("Dependant service not available"))
+           .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
+           .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
+         .end()
+        .endRest();
+
+        rest("/rebuild")
+        .post()
+        .route()
+         .circuitBreaker()
+          .to("bean://queryutils?method=prepareRebuild")
+          .marshal(jaxbDataFormat)
+          .to("activemq2:queue:orderCommand?jmsMessageType=Text")
+          .to("bean://queryutils?method=decodeRebuild")
+          .setHeader(Exchange.HTTP_RESPONSE_CODE, simple("200"))
+          .setBody(constant("Done"))
+         .onFallback()
+           .setBody(constant("Dependant service not available"))
+           .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
+           .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
+         .end()
+        .endRest();
        
     }
 
