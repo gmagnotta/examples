a1cb13a4e463c3e430fd88bf44c89d915f5739cc
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2021-11-29 23:00:35 +0100

    updated for flyway

--- quarkus-order-processor/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO items(id, description, price) VALUES (0, 'item1', 0.5);
-INSERT INTO items(id, description, price) VALUES (1, 'item1', 1.0);
-INSERT INTO items(id, description, price) VALUES (2, 'item2', 2.0);
-INSERT INTO items(id, description, price) VALUES (3, 'item3', 3.0);
-INSERT INTO items(id, description, price) VALUES (4, 'item4', 4.0);
-INSERT INTO items(id, description, price) VALUES (5, 'item5', 5.0);
---
+++ quarkus-order-processor/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+flyway -user=$USERNAME -password=$PASSWORD -url=$JDBC_URL $@
