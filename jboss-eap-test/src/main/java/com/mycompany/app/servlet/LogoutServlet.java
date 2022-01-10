2257fb23cd8841a9526a169cbda64b9b906b4470
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2022-01-10 17:58:26 +0100

    include servlet

---
+++ jboss-eap-test/src/main/java/com/mycompany/app/servlet/LogoutServlet.java
@@ -0,0 +1,24 @@
+package com.mycompany.app.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogoutServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		request.logout();
+
+		response.sendRedirect("/index.html");
+
+	}
+
+}
