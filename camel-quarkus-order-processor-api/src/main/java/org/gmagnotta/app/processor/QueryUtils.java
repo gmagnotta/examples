b19394ca13d578daca510fc092af2dbba68160a7
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2022-09-21 10:25:03 +0200

    remove legacy code

--- camel-quarkus-order-processor-api/src/main/java/org/gmagnotta/app/processor/QueryUtils.java
+++ camel-quarkus-order-processor-api/src/main/java/org/gmagnotta/app/processor/QueryUtils.java
@@ -29,37 +29,6 @@ public class QueryUtils {
 
 	}
 
-	/*public void decodeGetTopOrders(Exchange exchange) throws Exception {
-
-		String responseContent = exchange.getIn().getBody(String.class);
-        	
-		OrderCommandResponse responseMessage = Utils.unmarshall(OrderCommandResponse.class, responseContent);
-		List<TopValue> result = responseMessage.getTopvalue();
-		
-		Aggregationtype aggregation = new Aggregationtype();
-		
-		Iterator<TopValue> iterator = result.iterator();
-		while (iterator.hasNext()) {
-			
-			TopValue temp = iterator.next();
-			
-			Aggregatedordertype order = new Aggregatedordertype();
-			
-			int items = temp.getId();
-			
-			int orderId = temp.getValue();
-			
-			order.setItems(BigInteger.valueOf(items));
-			order.setOrderid(String.valueOf(orderId));
-			
-			aggregation.getAggregatedorder().add(order);
-			
-		}
-		
-		exchange.getOut().setBody(aggregation);
-	}*/
-
-	
 	public void prepareGetTopItems(Exchange exchange) throws Exception {
 
         TopItemsRequest request = new TopItemsRequest();
@@ -69,81 +38,4 @@ public class QueryUtils {
 
 	}
 
-	/*public void decodeGetTopItems(Exchange exchange) throws Exception {
-    	
-    	
-        String responseContent = exchange.getIn().getBody(String.class);
-        	
-		OrderCommandResponse responseMessage = Utils.unmarshall(OrderCommandResponse.class, responseContent);
-		
-		List<TopValue> result = responseMessage.getTopvalue();
-		
-		Aggregationtype aggregation = new Aggregationtype();
-		
-		Iterator<TopValue> iterator = result.iterator();
-		while (iterator.hasNext()) {
-			
-			TopValue temp = iterator.next();
-			
-			Aggregateditemtype order = new Aggregateditemtype();
-			
-			int items = temp.getId();
-			
-			int qty = temp.getValue();
-			
-			order.setItem(String.valueOf(items));
-			order.setQuantity(BigInteger.valueOf(qty));
-			
-			aggregation.getAggregateditem().add(order);
-			
-		}
-		
-		exchange.getOut().setBody(aggregation);
-		
-    }*/
-	
-	public void prepareReset(Exchange exchange) throws Exception {
-
-        /*OrderCommandRequest request = new OrderCommandRequest();
-        request.setOrderCommandEnum(OrderCommandRequestEnum.RESET);
-        
-        // replace the body of the exchange
-        exchange.getIn().setBody(request);*/
-
-	}
-
-	public void decodeReset(Exchange exchange) throws Exception {
-    	
-       /* String responseContent = exchange.getIn().getBody(String.class);
-
-		OrderCommandResponse responseMessage = Utils.unmarshall(OrderCommandResponse.class, responseContent);
-        	
-		if (!"200".equalsIgnoreCase(responseMessage.getStatus())) {
-			throw new Exception("Error");
-		} */
-
-	}
-	
-	public void prepareRebuild(Exchange exchange) throws Exception {
-
-        /*OrderCommandRequest request = new OrderCommandRequest();
-        request.setOrderCommandEnum(OrderCommandRequestEnum.REBUILD);
-        
-        // replace the body of the exchange
-        exchange.getIn().setBody(request);*/
-
-	}
-
-	public void decodeRebuild(Exchange exchange) throws Exception {
-    	
-        /*String responseContent = exchange.getIn().getBody(String.class);
-
-		OrderCommandResponse responseMessage = Utils.unmarshall(OrderCommandResponse.class, responseContent);
-        	
-		if (!"200".equalsIgnoreCase(responseMessage.getStatus())) {
-			throw new Exception("Error");
-		}*/
-
-	}
-	
 }
