e66c381acd675be0896cafabc89a0be35fed555b
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2022-09-23 17:08:39 +0200

    generate datagrid.proto

--- quarkus-datagrid/src/main/java/org/gmagnotta/protobuf/OrderInitializer.java
+++ quarkus-datagrid/src/main/java/org/gmagnotta/protobuf/OrderInitializer.java
@@ -8,6 +8,6 @@ import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder;
 import org.infinispan.protostream.types.java.math.BigDecimalAdapter;
 import org.infinispan.protostream.types.java.math.BigIntegerAdapter;
 
-@AutoProtoSchemaBuilder(includeClasses = {Order.class, LineItem.class, Item.class, BigDecimalAdapter.class, BigIntegerAdapter.class },  schemaFileName = "order.proto", schemaFilePath = "proto", schemaPackageName = "library")
+@AutoProtoSchemaBuilder(includeClasses = {Order.class, LineItem.class, Item.class, BigDecimalAdapter.class, BigIntegerAdapter.class },  schemaFileName = "datagrid.proto", schemaFilePath = "proto", schemaPackageName = "library")
 public interface OrderInitializer extends SerializationContextInitializer {
 }
