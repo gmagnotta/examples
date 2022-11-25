1986cff76cecafed0e7e6bf47ba2d1aebcf1ffa8
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2022-11-25 13:16:19 +0100

    implemented logout

--- hello-tomcat/src/main/java/com/gmagnotta/hello_tomcat/UserBean.java
+++ hello-tomcat/src/main/java/com/gmagnotta/hello_tomcat/UserBean.java
@@ -1,12 +1,16 @@
 package com.gmagnotta.hello_tomcat;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.security.Principal;
 import java.text.MessageFormat;
 import java.util.ResourceBundle;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 @Named
 @SessionScoped
@@ -26,11 +30,12 @@ public class UserBean implements Serializable {
     }
 
     public String getGreeting() {
-        
+
         String tempUser;
         if (name.length() == 0) {
 
-            ResourceBundle bundle = ResourceBundle.getBundle("messages", FacesContext.getCurrentInstance().getViewRoot().getLocale());
+            ResourceBundle bundle = ResourceBundle.getBundle("messages",
+                    FacesContext.getCurrentInstance().getViewRoot().getLocale());
             tempUser = bundle.getString("user");
 
         } else {
@@ -38,11 +43,53 @@ public class UserBean implements Serializable {
             tempUser = name;
 
         }
-            
-        ResourceBundle bundle = ResourceBundle.getBundle("messages", FacesContext.getCurrentInstance().getViewRoot().getLocale());
+
+        ResourceBundle bundle = ResourceBundle.getBundle("messages",
+                FacesContext.getCurrentInstance().getViewRoot().getLocale());
         String greet = bundle.getString("greet");
 
         return MessageFormat.format(greet, tempUser);
 
     }
+
+    public boolean isLogged() {
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
+                .getRequest();
+
+        return (request.getUserPrincipal() != null);
+    }
+
+    public String getUsername() {
+
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
+                .getRequest();
+
+        Principal principal = request.getUserPrincipal();
+
+        if (principal != null) {
+            return principal.getName();
+        }
+
+        return "UNKNOWN";
+
+    }
+
+    public void logout() throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
+                .getRequest();
+
+        request.logout();
+
+        //FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+
+        //FacesContext.getCurrentInstance().getExternalContext().responseSendError(401, "You are logged out.");
+
+        FacesContext.getCurrentInstance().getExternalContext().setResponseStatus(401);
+        FacesContext.getCurrentInstance().getExternalContext().getResponseOutputWriter().write("<html><head><meta http-equiv='refresh' content='0;/index.xhtml'></head></html>");
+        
+        FacesContext.getCurrentInstance().responseComplete();
+        //return "/index.xhtml?faces-redirect=true";
+    }
+
 }
