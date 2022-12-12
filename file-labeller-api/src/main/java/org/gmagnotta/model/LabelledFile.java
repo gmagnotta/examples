d1838dcfacf0863cdb096b37ae6d24652c410edc
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2022-12-12 09:03:11 +0100

    updates for native compilation

--- file-labeller-api/src/main/java/org/gmagnotta/model/LabelledFile.java
+++ file-labeller-api/src/main/java/org/gmagnotta/model/LabelledFile.java
@@ -2,6 +2,9 @@ package org.gmagnotta.model;
 
 import java.util.List;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public class LabelledFile {
 
     public String name;
