d2d95402ccb3c5cb10da532a854680e81fe3920c
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2023-07-19 17:55:12 +0200

    fixes

--- hello-tomcat/src/main/java/com/gmagnotta/hello_tomcat/ContextListenerImpl.java
+++ hello-tomcat/src/main/java/com/gmagnotta/hello_tomcat/ContextListenerImpl.java
@@ -1,20 +1,7 @@
 package com.gmagnotta.hello_tomcat;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.enterprise.inject.Produces;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-
-import org.apache.cxf.jaxrs.client.ClientConfiguration;
-import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
-import org.apache.cxf.jaxrs.client.WebClient;
-import org.apache.cxf.transport.http.HTTPConduit;
-
-import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
-import com.gmagnotta.hello_tomcat.service.BrokerService;
-import com.gmagnotta.hello_tomcat.service.FileLabellerService;
 
 public class ContextListenerImpl implements ServletContextListener {
 
