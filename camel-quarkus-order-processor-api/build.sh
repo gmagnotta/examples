ee9d049f0afcfbcaf1056f309d3aff9d5287de0a
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2023-12-29 14:07:17 +0100

    improvements

--- camel-quarkus-order-processor-api/build.sh
+++ camel-quarkus-order-processor-api/build.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
 
-export BUILDER_IMAGE="registry.access.redhat.com/ubi8/openjdk-11"
+export BUILDER_IMAGE="registry.access.redhat.com/ubi8/openjdk-17"
+export RUNTIME_IMAGE=$BUILDER_IMAGE
 export OUTPUT_IMAGE="camel-quarkus-order-processor-api"
 export INCREMENTAL=true
-#RUNTIME_IMAGE=$BUILDER_IMAGE
-#RUNTIME_ARTIFACT="/deployments/"
+export RUNTIME_ARTIFACT="/deployments/"
 
 buildah_s2i.sh
+buildah_s2i_runtime.sh
--- hello-tomcat/build.sh
+++ hello-tomcat/build.sh
@@ -4,8 +4,8 @@ set -e
 export BUILDER_IMAGE="registry.redhat.io/jboss-webserver-5/jws57-openjdk11-openshift-rhel8"
 export OUTPUT_IMAGE="hello-tomcat"
 export INCREMENTAL=true
-#export RUNTIME_IMAGE=$BUILDER_IMAGE
-#export RUNTIME_ARTIFACT="/deployments/hello-tomcat.war"
-##export RUNTIME_DEST_ARTIFACT="/deployments/ROOT.war"
+export RUNTIME_IMAGE=$BUILDER_IMAGE
+export RUNTIME_ARTIFACT="/deployments/"
 
 buildah_s2i.sh
+buildah_s2i_runtime.sh
