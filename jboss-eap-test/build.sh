672604c2e55edf64e8d3273fa1ccd96b0e02b0f2
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2023-12-30 12:02:35 +0100

    improvements

--- jboss-eap-test/build.sh
+++ jboss-eap-test/build.sh
@@ -5,9 +5,10 @@ set -e
 export BUILDER_IMAGE="registry.redhat.io/jboss-eap-7/eap74-openjdk11-openshift-rhel8"
 export OUTPUT_IMAGE="jboss-test"
 export INCREMENTAL=true
-#export RUNTIME_IMAGE="registry.redhat.io/jboss-eap-7/eap74-openjdk11-runtime-openshift-rhel8"
-#export RUNTIME_ARTIFACT="/s2i-output/server/"
-#export RUNTIME_DEST_ARTIFACT="/opt/eap"
-#export RUNTIME_CMD="/opt/eap/bin/openshift-launch.sh"
+export RUNTIME_IMAGE="registry.redhat.io/jboss-eap-7/eap74-openjdk11-runtime-openshift-rhel8"
+export RUNTIME_ARTIFACT="/s2i-output/server/"
+export DESTINATION_URL="/opt/eap"
+export RUNTIME_CMD="/opt/eap/bin/openshift-launch.sh"
 
 buildah_s2i.sh
+#buildah_s2i_runtime.sh
