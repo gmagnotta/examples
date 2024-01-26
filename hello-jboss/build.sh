cf8c8ef0175e86686220974d720c978b910ef86e
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2024-01-26 12:20:39 +0100

    improvements for local testing

--- hello-jboss/build.sh
+++ hello-jboss/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export BUILDER_IMAGE="registry.redhat.io/jboss-eap-7/eap74-openjdk11-openshift-rhel8"
+export BUILDER_IMAGE="registry.redhat.io/jboss-eap-7/eap74-openjdk11-openshift-rhel8@sha256:df517fcc8c950f2abefa2106917f3a1a5eba335072cd3e88e99268f2500e5e8f"
 export OUTPUT_IMAGE="hello-jboss"
 export INCREMENTAL=true
 export RUNTIME_IMAGE="registry.redhat.io/jboss-eap-7/eap74-openjdk11-runtime-openshift-rhel8"
@@ -12,3 +12,18 @@ export RUNTIME_CMD="/opt/eap/bin/openshift-launch.sh"
 
 buildah_s2i.sh
 #buildah_s2i_runtime.sh
+
+cat <<EOF > initdb.sh
+#!/bin/sh
+
+psql -f /opt/app-root/src/postgresql-start/V1.0.0__Initial_Schema.sql $POSTGRESQL_DATABASE
+EOF
+
+cat <<EOF > Containerfile
+FROM registry.redhat.io/rhel8/postgresql-10:1-232
+
+COPY src/main/resources/db/ /opt/app-root/src/postgresql-start/
+COPY initdb.sh /opt/app-root/src/postgresql-start/
+EOF
+
+podman build -f Containerfile . -t hello-jbossdb
--- hello-jboss/run.sh
+++ hello-jboss/run.sh
@@ -1,33 +1,47 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
 
-podman run --rm -ti -p 8080:8080 \
--e DB_SERVICE_PREFIX_MAPPING=ocp-postgresql=DS1 \
--e OCP_POSTGRESQL_SERVICE_HOST=192.168.1.6 \
--e OCP_POSTGRESQL_SERVICE_PORT=5432 \
--e DS1_CONNECTION_CHECKER=org.jboss.jca.adapters.jdbc.extensions.postgres.PostgreSQLValidConnectionChecker \
--e DS1_DATABASE=jboss \
--e DS1_DRIVER=postgresql \
--e DS1_EXCEPTION_SORTER=org.jboss.jca.adapters.jdbc.extensions.postgres.PostgreSQLExceptionSorter \
--e DS1_USERNAME=jboss \
--e DS1_PASSWORD=jboss \
--e DS1_MAX_POOL_SIZE=20 \
--e DS1_MIN_POOL_SIZE=20 \
--e DS1_NONXA=true \
--e DS1_DATABASE=jboss \
--e DS1_USERNAME=jboss \
--e DS1_PASSWORD=jboss \
--e ENABLE_GENERATE_DEFAULT_DATASOURCE="" \
--e MQ_USERNAME="amq" \
--e MQ_PASSWORD="amq" \
--e MQ_QUEUES="getTopItemsCommand,getTopOrdersCommand,createOrderCommand,invalidMessage" \
--e MQ_TOPICS="" \
--e MQ_SERVICE_PREFIX_MAPPING="test-amq7=MQ" \
--e MQ_PROTOCOL="tcp" \
--e JBOSS_EAP_AMQ_TCP_SERVICE_HOST="192.168.1.6" \
--e JBOSS_EAP_AMQ_TCP_SERVICE_PORT="61616" \
--e TEST_AMQ_TCP_SERVICE_HOST="192.168.1.6" \
--e TEST_AMQ_TCP_SERVICE_PORT="61616" \
--e MQ_SERIALIZABLE_PACKAGES="" \
--e MQ_JNDI="java:jboss/DefaultJMSConnectionFactory" \
-localhost/hello-jboss
+podman network create --ignore dev
+
+podman run -d --name jbossdb --net dev \
+ -e POSTGRESQL_USER="jboss" \
+ -e POSTGRESQL_PASSWORD="jboss" \
+ -e POSTGRESQL_DATABASE="jboss" \
+ localhost/hello-jbossdb
+
+podman run --name jboss --net dev \
+ -e SSO_URL="http://keycloak:8081/" \
+ -e SSO_REALM="jboss" \
+ -e SSO_SECRET="h301iicLztkcTeuIxOHyNtk8VViCrGp4" \
+ -e SSO_CLIENT="jboss" \
+ -e HOSTNAME_HTTP="localhost" \
+ -e SSO_DISABLE_SSL_CERTIFICATE_VALIDATION="true" \
+ -e DB_SERVICE_PREFIX_MAPPING="ocp-postgresql=DS1" \
+ -e OCP_POSTGRESQL_SERVICE_HOST="jbossdb" \
+ -e OCP_POSTGRESQL_SERVICE_PORT="5432" \
+ -e DS1_CONNECTION_CHECKER="org.jboss.jca.adapters.jdbc.extensions.postgres.PostgreSQLValidConnectionChecker" \
+ -e DS1_DATABASE="jboss" \
+ -e DS1_DRIVER="postgresql" \
+ -e DS1_EXCEPTION_SORTER="org.jboss.jca.adapters.jdbc.extensions.postgres.PostgreSQLExceptionSorter" \
+ -e DS1_USERNAME="jboss" \
+ -e DS1_PASSWORD="jboss" \
+ -e DS1_MAX_POOL_SIZE="20" \
+ -e DS1_MIN_POOL_SIZE="20" \
+ -e DS1_NONXA="true" \
+ -e DS1_DATABASE="jboss" \
+ -e DS1_USERNAME="jboss" \
+ -e DS1_PASSWORD="jboss" \
+ -e ENABLE_GENERATE_DEFAULT_DATASOURCE="" \
+ -e MQ_USERNAME="amq" \
+ -e MQ_PASSWORD="amq" \
+ -e MQ_QUEUES="getTopItemsCommand,getTopOrdersCommand,createOrderCommand,invalidMessage" \
+ -e MQ_TOPICS="" \
+ -e MQ_SERVICE_PREFIX_MAPPING="test-amq7=MQ" \
+ -e MQ_PROTOCOL="tcp" \
+ -e JBOSS_EAP_AMQ_TCP_SERVICE_HOST="amqbroker" \
+ -e JBOSS_EAP_AMQ_TCP_SERVICE_PORT="61616" \
+ -e TEST_AMQ_TCP_SERVICE_HOST="amqbroker" \
+ -e TEST_AMQ_TCP_SERVICE_PORT="61616" \
+ -e MQ_SERIALIZABLE_PACKAGES="" \
+ -e MQ_JNDI="java:jboss/DefaultJMSConnectionFactory" \
+ -p 8080:8080 \
+ localhost/hello-jboss
