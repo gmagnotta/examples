04fa07eed6b83d5205393e368feecc24d16f15a2
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2024-02-16 16:25:43 +0100

    improved run file

--- hello-jboss/run.sh
+++ hello-jboss/run.sh
@@ -10,13 +10,26 @@ podman run -d --rm --name jbossdb --net dev \
 
 sleep 15
 
-podman run --name jboss --net dev \
+if [ ! -f eapkeystore.jks ]; then
+  echo "Generate keystore"
+  keytool -genkeypair -storepass password -keypass password -dname CN=localhost -alias https -storetype JKS -keyalg RSA -keystore eapkeystore.jks
+fi
+
+if [ ! -f sso.pem ]; then
+  echo "Import SSO cert"
+  /usr/bin/openssl s_client -showcerts -connect keycloak:8444 </dev/null 2>/dev/null|openssl x509 -outform PEM >sso.pem
+
+  keytool -importcert -alias sso -storepass password -noprompt -storetype JKS -keystore sso.jks -file sso.pem
+fi
+
+podman run --rm --name jboss --net dev \
  -v ./sso.jks:/etc/sso-secret-volume/sso.jks:z \
- -e KEYCLOAK_PROVIDER_URL="https://keycloak:8443/realms/jboss" \
+ -v ./eapkeystore.jks:/etc/eap-secret-volume/eapkeystore.jks:z \
+ -e KEYCLOAK_PROVIDER_URL="http://10.89.0.52:8081/realms/jboss" \
  -e KEYCLOAK_TRUSTSTORE="/etc/sso-secret-volume/sso.jks" \
- -e KEYCLOAK_TRUSTSTORE_PASSWORD="ssopassword" \
- -e KEYCLOAK_CLIENTID="hello" \
- -e KEYCLOAK_SECRET="g1aP3w9uvS6yQWfxnvTgKynZYylwF8uZ" \
+ -e KEYCLOAK_TRUSTSTORE_PASSWORD="password" \
+ -e KEYCLOAK_CLIENTID="jboss" \
+ -e KEYCLOAK_SECRET="j5ILKb1uhkrIWJ5N0CQ2USqcUm5GYy4F" \
  -e POSTGRESQL_SERVICE_HOST="jbossdb" \
  -e POSTGRESQL_SERVICE_PORT="5432" \
  -e POSTGRESQL_USER="jboss" \
@@ -36,5 +49,11 @@ podman run --name jboss --net dev \
  -e TEST_AMQ_TCP_SERVICE_PORT="61616" \
  -e MQ_SERIALIZABLE_PACKAGES="" \
  -e MQ_JNDI="java:jboss/DefaultJMSConnectionFactory" \
+ -e HTTPS_KEYSTORE_DIR="/etc/eap-secret-volume" \
+ -e HTTPS_KEYSTORE="eapkeystore.jks" \
+ -e HTTPS_KEYSTORE_TYPE="JKS" \
+ -e HTTPS_NAME="https" \
+ -e HTTPS_PASSWORD="password" \
  -p 8080:8080 \
+ -p 8443:8443 \
  localhost/hello-jboss
