aeeea1398e4c958a029dfeb458e2d2260c520713
Giuseppe Magnotta <giuseppe.magnotta@gmail.com>
2024-03-01 15:57:41 +0100

    included ping over jms + openapi

--- camel-quarkus-order-processor-api/run.sh
+++ camel-quarkus-order-processor-api/run.sh
@@ -3,7 +3,10 @@
 podman network create --ignore dev
 
 podman run --rm --name camel-quarkus-order-processor-api --net dev \
- -e QUARKUS_HTTP_PORT=8090 \
+ -e QUARKUS_HTTP_PORT=8080 \
  -e APP_WEBSERVICE_SOAP_URL="http://jboss:8080/soap" \
- -p 8090:8090 \
+ -e QUARKUS_ARTEMIS_URL=tcp://amqbroker:61616 \
+ -e QUARKUS_ARTEMIS_USERNAME=amq \
+ -e QUARKUS_ARTEMIS_PASSWORD=amq \
+ -p 8090:8080 \
  localhost/camel-quarkus-order-processor-api
--- camel-quarkus-order-processor-api/src/main/java/org/gmagnotta/app/route/RestRoute.java
+++ camel-quarkus-order-processor-api/src/main/java/org/gmagnotta/app/route/RestRoute.java
@@ -1,20 +1,22 @@
 package org.gmagnotta.app.route;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.activemq.ActiveMQComponent;
 import org.apache.camel.component.jsonb.JsonbDataFormat;
+import org.apache.camel.converter.jaxb.JaxbDataFormat;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.apache.camel.model.rest.RestParamType;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import resource.app.mycompany.com.soapresource.GetTopOrders;
-import resource.app.mycompany.com.soapresource.GetTopItems;
 import resource.app.mycompany.com.soapresource.GetItems;
-
-import org.apache.camel.converter.jaxb.JaxbDataFormat;
+import resource.app.mycompany.com.soapresource.GetTopItems;
+import resource.app.mycompany.com.soapresource.GetTopOrders;
 
 /**
  * Camel Route to expose Rest APIs
@@ -24,6 +26,18 @@ public class RestRoute extends RouteBuilder {
 
   @ConfigProperty(name = "app.webservice.soap.url")
   private String soapurl;
+  
+  @ConfigProperty(name = "quarkus.artemis.url")
+  String brokerUrl;
+
+  @ConfigProperty(name = "quarkus.artemis.username")
+  String brokerUsername;
+
+  @ConfigProperty(name = "quarkus.artemis.password")
+  String brokerPassword;
+
+  @Produces
+  ActiveMQConnectionFactory activeMQConectionFactory; // needed to trigger creation with produces
 
   @Override
   public void configure() throws Exception {
@@ -31,6 +45,13 @@ public class RestRoute extends RouteBuilder {
     JaxbDataFormat soapDataFormat = new JaxbDataFormat("resource.app.mycompany.com.soapresource");
 
     JsonbDataFormat jsonDataFormat = new JsonbDataFormat();
+
+    activeMQConectionFactory = new ActiveMQConnectionFactory(brokerUsername, brokerPassword, brokerUrl);
+    	
+    ActiveMQComponent activeMq = new ActiveMQComponent();
+    activeMq.setConnectionFactory(activeMQConectionFactory);
+    
+    getContext().addComponent("activemq", activeMq);
 
     restConfiguration()
         .component("platform-http")
@@ -44,6 +65,8 @@ public class RestRoute extends RouteBuilder {
           .to("direct:topItems")
         .get("/items")
           .to("direct:items")
+        .get("/ping")
+          .to("direct:ping")
         .post("/generate").param().name("amount").type(RestParamType.query).defaultValue("1").endParam()
           .to("direct:generate");
 
@@ -137,6 +160,23 @@ public class RestRoute extends RouteBuilder {
       .setHeader(Exchange.HTTP_RESPONSE_CODE, simple("201"))
       .setBody(constant("Done"))
       .to("bean://generateprocessor")
+      .end();
+
+    from("direct:ping")
+      .routeId("ping")
+      .setBody(constant("PING"))
+      // .log("REST 2 SOAP : SOAP request payload")
+      // .log(body().toString())
+      .circuitBreaker()
+        .toD("activemq:queue:pingCommand?jmsMessageType=Text")
+        // .log("REST 2 SOAP : SOAP response")
+        // .log(body().toString())
+        .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
+      .onFallback()
+        .setBody(constant("Dependant service not available"))
+        .setHeader(Exchange.CONTENT_TYPE, constant("text/plain"))
+        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
+      .endCircuitBreaker()
       .end();
 
   }
